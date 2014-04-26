d708aa4b05c59787367524f6f8dec458211396d1
Jos Elstgeest <jos@tidi.nl>
2014-04-26 23:36:02 +0200

    import classers

--- lib/WideImage/Operation/Exception/InvalidFitMethodException.php
+++ lib/WideImage/Operation/Exception/InvalidFitMethodException.php
@@ -2,9 +2,11 @@
 
 namespace WideImage\Operation\Exception;
 
+use WideImage\Exception\Exception;
+
 /**
  * An Exception for when an invalid fit method is passed
  *
  * @package Internal/Operations
  */
-class InvalidFitMethodException extends \WideImage\Exception\Exception {}
+class InvalidFitMethodException extends Exception {}
--- lib/WideImage/Operation/Exception/InvalidResizeDimensionException.php
+++ lib/WideImage/Operation/Exception/InvalidResizeDimensionException.php
@@ -2,9 +2,11 @@
 
 namespace WideImage\Operation\Exception;
 
+use WideImage\Exception\Exception;
+
 /**
  * An Exception for when an invalid resize dimensions are passed
  *
  * @package Internal/Operations
  */
-class InvalidResizeDimensionException extends \WideImage\Exception\Exception {}
+class InvalidResizeDimensionException extends Exception {}
